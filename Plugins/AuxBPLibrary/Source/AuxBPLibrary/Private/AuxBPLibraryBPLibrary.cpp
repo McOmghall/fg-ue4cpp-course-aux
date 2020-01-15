810f1633a607834b41cfdfbc63e9987ad9776709
Pedro Montoto García <pmgberm@gmail.com>
2020-01-15 19:26:36 +0100

    Added Example Blueprint function library, Corrected Door.h for building shipping

--- Plugins/AuxBPLibrary/Source/AuxBPLibrary/Private/AuxBPLibraryBPLibrary.cpp
+++ Plugins/AuxBPLibrary/Source/AuxBPLibrary/Private/AuxBPLibraryBPLibrary.cpp
@@ -1,6 +1,5 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
-#include "Engine/Classes/Kismet/KismetArrayLibrary.h"
 #include "AuxBPLibraryBPLibrary.h"
 #include "AuxBPLibrary.h"
 
